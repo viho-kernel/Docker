76481d635326c1a8db197b5cad437abf67353ac3
viho-kernel <vihari.reddy1802@gmail.com>
2026-03-31 12:20:15 +0530

    latest commit

--- Java/App.java
+++ Java/App.java
@@ -1,7 +1,5 @@
-package com.example;
-
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Java!");
+        System.out.println("Hello from Java in Docker!");
     }
 }
